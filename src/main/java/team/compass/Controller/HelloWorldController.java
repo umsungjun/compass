4b8acfeac8da1093208d8a0394417ded123d4978
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-03-30 12:47:11 +0900

    controller test

--- src/main/java/team/compass/Controller/HelloWorldController.java
+++ src/main/java/team/compass/Controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String test() {
-        return "제로베이스 1석 4조 화이팅";
+        return "제로베이스 2023 03 29 Test";
     }
 }
